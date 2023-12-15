d998b58c939e27fe8018cfa912fe0348e320f3fd
cubewhy <me@lunarclient.top>
2023-12-15 18:08:48 +0800

    join game args

--- src/main/java/org/cubewhy/celestial/utils/FileUtils.java
+++ src/main/java/org/cubewhy/celestial/utils/FileUtils.java
@@ -1,7 +1,15 @@
 package org.cubewhy.celestial.utils;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 public class FileUtils {
     private FileUtils() {
@@ -11,7 +19,37 @@ public class FileUtils {
         return FileUtils.class.getResourceAsStream(path);
     }
 
-    public static byte[] readBytes(InputStream inputStream) throws IOException {
+    public static byte[] readBytes(@NotNull InputStream inputStream) throws IOException {
         return inputStream.readAllBytes();
+    }
+
+    public static void unzipNatives(File nativesZip, String baseDir) throws IOException {
+        File dir = new File(baseDir, "natives");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        unZip(nativesZip, dir);
+    }
+
+    public static void unZip(File input, File outputDir) throws IOException {
+        ZipFile zipfile = new ZipFile(input);
+        ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(input.toPath()));
+
+        ZipEntry entry;
+        while ((entry = zipInputStream.getNextEntry()) != null) {
+            File out = new File(outputDir, entry.getName());
+            if (entry.isDirectory()) {
+                out.mkdirs();
+            } else {
+                out.createNewFile();
+                InputStream entryInputStream = zipfile.getInputStream(entry);
+                try (FileOutputStream fileOutPutStream = new FileOutputStream(out)) {
+                    int b;
+                    while ((b = entryInputStream.read()) != -1) {
+                        fileOutPutStream.write(b);
+                    }
+                }
+            }
+        }
     }
 }
