dad7aaa9bdbd6eac064cc292e9aea80e4bb450e5
cubewhy <me@lunarclient.top>
2023-12-17 15:09:48 +0800

    fix error

--- src/main/java/org/cubewhy/celestial/event/impl/AuthEvent.java
+++ src/main/java/org/cubewhy/celestial/event/impl/AuthEvent.java
@@ -9,24 +9,29 @@ package org.cubewhy.celestial.event.impl;
 import org.cubewhy.celestial.event.Event;
 
 import java.net.URL;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AuthEvent extends Event {
 
     public final URL authURL;
-    private volatile String result = null;
+    private String result = "";
+    private final AtomicBoolean responded = new AtomicBoolean(false);
 
     public AuthEvent(URL authURL) {
         this.authURL = authURL;
     }
 
     public String waitForAuth() {
-        while (result == null) {
+        while (!responded.get()) {
             Thread.onSpinWait();
         }
         return result;
     }
 
     public void put(String url) {
-        this.result = url;
+        if (url != null) {
+            this.result = url;
+        }
+        this.responded.set(true);
     }
 }
