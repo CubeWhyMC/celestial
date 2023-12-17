9e8121d5ba4a29efb3a345f78ca48d2a2d7e0935
cubewhy <me@lunarclient.top>
2023-12-17 12:21:29 +0800

    border for GuiSettings

--- src/main/java/org/cubewhy/celestial/gui/pages/GuiSettings.java
+++ src/main/java/org/cubewhy/celestial/gui/pages/GuiSettings.java
@@ -1,10 +1,14 @@
 package org.cubewhy.celestial.gui.pages;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+
+import static org.cubewhy.celestial.Celestial.f;
 
 public class GuiSettings extends JPanel {
     public GuiSettings() {
+        this.setBorder(new TitledBorder(null, f.getString("gui.version.title"), TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, Color.orange));
         this.setLayout(new GridLayout(3, 3, 5, 5));
         this.initGui();
     }
