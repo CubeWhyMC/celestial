0d537ac3dbd4fc8045b8683a8cfd317a9a4d3762
cubewhy <me@lunarclient.top>
2023-12-21 19:57:04 +0800

    use JLabel

--- src/main/java/org/cubewhy/celestial/gui/elements/GuiVersionSelect.java
+++ src/main/java/org/cubewhy/celestial/gui/elements/GuiVersionSelect.java
@@ -7,8 +7,16 @@
 package org.cubewhy.celestial.gui.elements;
 
 import com.google.gson.JsonObject;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.cubewhy.celestial.Celestial;
+import org.cubewhy.celestial.event.impl.GameStartEvent;
+import org.cubewhy.celestial.event.impl.GameTerminateEvent;
+import org.cubewhy.celestial.utils.CrashReportType;
+import org.cubewhy.celestial.utils.SystemUtils;
+import org.cubewhy.celestial.utils.TextUtils;
 import org.cubewhy.celestial.utils.lunar.LauncherData;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,6 +24,7 @@ import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -28,7 +37,7 @@ public class GuiVersionSelect extends JPanel {
 
     public GuiVersionSelect() throws IOException {
         this.setBorder(new TitledBorder(null, f.getString("gui.version-select.title"), TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, Color.orange));
-        this.setLayout(new GridLayout(3, 2, 5, 5));
+        this.setLayout(new GridLayout(4, 2, 5, 5));
 
         this.initGui();
     }
@@ -37,11 +46,11 @@ public class GuiVersionSelect extends JPanel {
         JComboBox<String> versionSelect = new JComboBox<>();
         JComboBox<String> moduleSelect = new JComboBox<>();
         JTextField branchInput = new JTextField();
-        this.add(new Label("Version"));
+        this.add(new JLabel(f.getString("gui.version-select.label.version")));
         this.add(versionSelect);
-        this.add(new Label("Module"));
+        this.add(new JLabel(f.getString("gui.version-select.label.module")));
         this.add(moduleSelect);
-        this.add(new Label("Branch"));
+        this.add(new JLabel(f.getString("gui.version-select.label.branch")));
         this.add(branchInput);
 
         // add items
@@ -78,6 +87,92 @@ public class GuiVersionSelect extends JPanel {
         }
         initInput(versionSelect, moduleSelect, branchInput);
         isFinishOk = true;
+
+        // add launch buttons
+        JButton btnOffline = new JButton(f.getString("gui.version.offline"));
+        this.add(btnOffline);
+        btnOffline.addActionListener(e -> {
+            try {
+                this.offline();
+            } catch (IOException | InterruptedException ex) {
+                String trace = TextUtils.dumpTrace(ex);
+                log.error(trace);
+            } catch (AttachNotSupportedException ignored) {
+                log.warn("Failed to attach to the game process");
+            }
+        });
+    }
+
+    private void offline() throws IOException, InterruptedException, AttachNotSupportedException {
+        if (gamePid != 0) {
+            if (SystemUtils.findJava(LauncherData.getMainClass(null)) != null) {
+                JOptionPane.showMessageDialog(this, f.getString("gui.version.launched.message"), f.getString("gui.version.launched.title"), JOptionPane.WARNING_MESSAGE);
+                return;
+            } else {
+                gamePid = 0;
+            }
+        }
+        ProcessBuilder process = Celestial.launch();
+        Process p = SystemUtils.callExternalProcess(process);
+        new Thread(() -> {
+            try {
+                int code = p.waitFor();
+                log.info("Game terminated");
+                Celestial.gamePid = 0;
+                new GameTerminateEvent().call();
+                if (code != 0) {
+                    // upload crash report
+                    log.info("Client looks crashed, starting upload the log");
+                    String trace = FileUtils.readFileToString(logFile, StandardCharsets.UTF_8);
+                    String script = FileUtils.readFileToString(launchScript, StandardCharsets.UTF_8);
+                    Map<String, String> map1 = launcherData.uploadCrashReport(trace, CrashReportType.GAME, script);
+                    if (!map1.isEmpty()) {
+                        String url = map1.get("url");
+                        String id = map1.get("id");
+                        JOptionPane.showMessageDialog(this, String.format("""
+                                        Your client was crashed:
+                                        Crash id: %s
+                                        View your crash report at %s
+                                        View the log of the latest launch: %s
+                                                                                
+                                        *%s*""", id, url, logFile.getPath(), f.getString("gui.version.crash.tip")), "Game crashed!", JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(this, String.format("""
+                                        Your client was crashed:
+                                        View the log of the latest launch: %s
+                                        *%s*
+                                        """, logFile.getPath(), f.getString("gui.version.crash.tip")));
+                    }
+                }
+            } catch (IOException | InterruptedException ex) {
+                String trace = TextUtils.dumpTrace(ex);
+                log.error(trace);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // find the game process
+            try {
+                Thread.sleep(3000); // sleep 10s
+            } catch (InterruptedException ignored) {
+
+            }
+            if (p.isAlive()) {
+                try {
+                    VirtualMachine java = SystemUtils.findJava(LauncherData.getMainClass(null));
+                    assert java != null;
+                    String id = java.id();
+                    gamePid = Long.parseLong(id);
+                    java.detach();
+                } catch (Exception ex) {
+                    log.error("Failed to get the real pid of the game, is Celestial launched non java program?");
+                    log.warn("process.pid() will be used to get the process id, which may not be the real PID");
+                    gamePid = p.pid();
+                }
+                log.info("Pid: " + gamePid);
+                new GameStartEvent(gamePid).call();
+            }
+        }).start();
     }
 
     private void initInput(@NotNull JComboBox<String> versionSelect, @NotNull JComboBox<String> moduleSelect, @NotNull JTextField branchInput) {
@@ -104,7 +199,7 @@ public class GuiVersionSelect extends JPanel {
     }
 
 
-    private void refreshModuleSelect(@NotNull JComboBox<String> versionSelect, JComboBox<String> moduleSelect, boolean reset) throws IOException {
+    private void refreshModuleSelect(@NotNull JComboBox<String> versionSelect, @NotNull JComboBox<String> moduleSelect, boolean reset) throws IOException {
         moduleSelect.removeAllItems();
         Map<String, Object> map = LauncherData.getSupportModules(metadata, (String) versionSelect.getSelectedItem());
         List<String> modules = (ArrayList<String>) map.get("modules");
--- src/main/java/org/cubewhy/celestial/gui/pages/GuiVersion.java
+++ src/main/java/org/cubewhy/celestial/gui/pages/GuiVersion.java
@@ -1,26 +1,14 @@
 package org.cubewhy.celestial.gui.pages;
 
-import com.sun.tools.attach.AttachNotSupportedException;
-import com.sun.tools.attach.VirtualMachine;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
-import org.cubewhy.celestial.Celestial;
-import org.cubewhy.celestial.event.impl.GameStartEvent;
-import org.cubewhy.celestial.event.impl.GameTerminateEvent;
 import org.cubewhy.celestial.gui.elements.GuiVersionSelect;
-import org.cubewhy.celestial.utils.CrashReportType;
-import org.cubewhy.celestial.utils.SystemUtils;
-import org.cubewhy.celestial.utils.TextUtils;
-import org.cubewhy.celestial.utils.lunar.LauncherData;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
-import static org.cubewhy.celestial.Celestial.*;
+import static org.cubewhy.celestial.Celestial.f;
 
 @Slf4j
 public class GuiVersion extends JPanel {
@@ -30,86 +18,6 @@ public class GuiVersion extends JPanel {
     }
 
     private void initGui() throws IOException {
-        JButton btnOffline = new JButton(f.getString("gui.version.offline"));
-        this.add(btnOffline);
         this.add(new GuiVersionSelect());
-
-        btnOffline.addActionListener(e -> {
-            try {
-                if (gamePid != 0) {
-                    if (SystemUtils.findJava(LauncherData.getMainClass(null)) != null) {
-                        JOptionPane.showMessageDialog(this, f.getString("gui.version.launched.message"), f.getString("gui.version.launched.title"), JOptionPane.WARNING_MESSAGE);
-                        return;
-                    } else {
-                        gamePid = 0;
-                    }
-                }
-                ProcessBuilder process = Celestial.launch();
-                Process p = SystemUtils.callExternalProcess(process);
-                new Thread(() -> {
-                    try {
-                        int code = p.waitFor();
-                        log.info("Game terminated");
-                        Celestial.gamePid = 0;
-                        new GameTerminateEvent().call();
-                        if (code != 0) {
-                            // upload crash report
-                            log.info("Client looks crashed, starting upload the log");
-                            String trace = FileUtils.readFileToString(logFile, StandardCharsets.UTF_8);
-                            String script = FileUtils.readFileToString(launchScript, StandardCharsets.UTF_8);
-                            Map<String, String> map = launcherData.uploadCrashReport(trace, CrashReportType.GAME, script);
-                            if (!map.isEmpty()) {
-                                String url = map.get("url");
-                                String id = map.get("id");
-                                JOptionPane.showMessageDialog(this, String.format("""
-                                        Your client was crashed:
-                                        Crash id: %s
-                                        View your crash report at %s
-                                        View the log of the latest launch: %s
-                                                                                
-                                        *%s*""", id, url, logFile.getPath(), f.getString("gui.version.crash.tip")), "Game crashed!", JOptionPane.ERROR_MESSAGE);
-                            } else {
-                                JOptionPane.showMessageDialog(this, String.format("""
-                                        Your client was crashed:
-                                        View the log of the latest launch: %s
-                                        *%s*
-                                        """, logFile.getPath(), f.getString("gui.version.crash.tip")));
-                            }
-                        }
-                    } catch (IOException | InterruptedException ex) {
-                        String trace = TextUtils.dumpTrace(ex);
-                        log.error(trace);
-                    }
-                }).start();
-
-                new Thread(() -> {
-                    // find the game process
-                    try {
-                        Thread.sleep(3000); // sleep 10s
-                    } catch (InterruptedException ignored) {
-
-                    }
-                    if (p.isAlive()) {
-                        try {
-                            VirtualMachine java = SystemUtils.findJava(LauncherData.getMainClass(null));
-                            assert java != null;
-                            String id = java.id();
-                            gamePid = Long.parseLong(id);
-                            java.detach();
-                        } catch (Exception ex) {
-                            log.error("Failed to get the real pid of the game, is Celestial launched non java program?");
-                            log.warn("process.pid() will be used to get the process id, which may not be the real PID");
-                            gamePid = p.pid();
-                        }
-                        log.info("Pid: " + gamePid);
-                        new GameStartEvent(gamePid).call();
-                    }
-                }).start();
-            } catch (IOException | InterruptedException ex) {
-                String trace = TextUtils.dumpTrace(ex);
-                log.error(trace);
-            } catch (AttachNotSupportedException ignored) {
-            }
-        });
     }
 }
