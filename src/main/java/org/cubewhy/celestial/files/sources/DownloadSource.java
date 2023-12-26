324448b92ef275673ad54398bd793cd7bfe45191
cubewhy <me@lunarclient.top>
2023-12-26 20:08:15 +0800

    complete minecraft textures

---
+++ src/main/java/org/cubewhy/celestial/files/sources/DownloadSource.java
@@ -0,0 +1,14 @@
+/*
+ * Celestial Launcher <me@lunarclient.top>
+ * License under GPLv3
+ * Do NOT remove this note if you want to copy this file.
+ */
+
+package org.cubewhy.celestial.files.sources;
+
+import java.net.URL;
+
+public interface DownloadSource {
+    // TODO complete this class
+    URL getBaseURL();
+}
