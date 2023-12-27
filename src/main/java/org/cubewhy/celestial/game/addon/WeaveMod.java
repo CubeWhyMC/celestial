410483410715a9df105816057ef80b8e4097712d
cubewhy <me@lunarclient.top>
2023-12-27 21:22:53 +0800

    class of mods

---
+++ src/main/java/org/cubewhy/celestial/game/addon/WeaveMod.java
@@ -0,0 +1,34 @@
+/*
+ * Celestial Launcher <me@lunarclient.top>
+ * License under GPLv3
+ * Do NOT remove this note if you want to copy this file.
+ */
+
+package org.cubewhy.celestial.game.addon;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public record WeaveMod(File file) {
+    public static final File modFolder = new File(System.getProperty("user.home"), ".weave/mods");
+
+    /**
+     * Find all mods in the lunarcn mods folder
+     * */
+    @NotNull
+    @Contract(pure = true)
+    public static List<WeaveMod> findAll() {
+        List<WeaveMod> list = new ArrayList<>();
+        for (File file : Objects.requireNonNull(modFolder.listFiles())) {
+            if (file.getName().endsWith(".jar") && file.isFile()) {
+                list.add(new WeaveMod(file));
+            }
+        }
+        return list;
+    }
+}
