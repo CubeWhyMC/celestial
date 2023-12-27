635c238707044aa62468feed1c5db46719f0a93c
cubewhy <me@lunarclient.top>
2023-12-27 21:05:01 +0800

    addon manager

---
+++ src/main/java/org/cubewhy/celestial/gui/elements/GuiAddonManager.java
@@ -0,0 +1,47 @@
+/*
+ * Celestial Launcher <me@lunarclient.top>
+ * License under GPLv3
+ * Do NOT remove this note if you want to copy this file.
+ */
+
+package org.cubewhy.celestial.gui.elements;
+
+import lombok.extern.slf4j.Slf4j;
+import org.cubewhy.celestial.game.JavaAgent;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
+import static org.cubewhy.celestial.Celestial.f;
+
+@Slf4j
+public class GuiAddonManager extends JPanel {
+    private final JTabbedPane tab = new JTabbedPane();
+
+    public GuiAddonManager() {
+        this.setBorder(new TitledBorder(null, f.getString("gui.addons.title"), TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, Color.orange));
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.initGui();
+    }
+
+    private void initGui() {
+        DefaultListModel<String> lunarCN = new DefaultListModel<>();
+        DefaultListModel<String> weave = new DefaultListModel<>();
+        DefaultListModel<String> agents = new DefaultListModel<>();
+        // load items
+        for (JavaAgent javaAgent : JavaAgent.findAll()) {
+            agents.addElement(javaAgent.getFile().getName());
+        }
+
+        JList<String> list1 = new JList<>(lunarCN);
+        JList<String> list2 = new JList<>(weave);
+        JList<String> list3 = new JList<>(agents);
+        tab.addTab(f.getString("gui.addons.mods.cn"), new JScrollPane(list1));
+        tab.addTab(f.getString("gui.addons.mods.weave"), new JScrollPane(list2));
+        tab.addTab(f.getString("gui.addons.agents"), new JScrollPane(list3));
+
+        this.add(tab);
+    }
+}
