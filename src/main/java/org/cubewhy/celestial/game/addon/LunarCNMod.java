8143be5d0fa8c24d2a29e91b7c2ec3677a803e5d
cubewhy <me@lunarclient.top>
2023-12-27 21:22:53 +0800

    class of mods

---
+++ src/main/java/org/cubewhy/celestial/game/addon/LunarCNMod.java
@@ -0,0 +1,35 @@
+/*
+ * Celestial Launcher <me@lunarclient.top>
+ * License under GPLv3
+ * Do NOT remove this note if you want to copy this file.
+ */
+
+package org.cubewhy.celestial.game.addon;
+
+import org.cubewhy.celestial.Celestial;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public record LunarCNMod(File file) {
+    public static final File modFolder = new File(Celestial.configDir, "mods");
+
+    /**
+     * Find all mods in the lunarcn mods folder
+     * */
+    @NotNull
+    @Contract(pure = true)
+    public static List<LunarCNMod> findAll() {
+        List<LunarCNMod> list = new ArrayList<>();
+        for (File file : Objects.requireNonNull(modFolder.listFiles())) {
+            if (file.getName().endsWith(".jar") && file.isFile()) {
+                list.add(new LunarCNMod(file));
+            }
+        }
+        return list;
+    }
+}
