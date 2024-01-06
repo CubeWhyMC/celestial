ac61a86ce7760077dca8d9789c2d1308c82582e3
cubewhy <me@lunarclient.top>
2024-01-06 11:43:21 +0800

    remove OperatingSystem.java

--- src/main/java/org/cubewhy/celestial/utils/ManagedProcess.java
+++ src/main/java/org/cubewhy/celestial/utils/ManagedProcess.java
@@ -1,5 +1,11 @@
 
-package org.jackhuang.hmcl.util.platform;
+/*
+ * Celestial Launcher <me@lunarclient.top>
+ * License under GPLv3
+ * Do NOT remove this note if you want to copy this file.
+ */
+
+package org.cubewhy.celestial.utils;
 
 import lombok.Getter;
 
