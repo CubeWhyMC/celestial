4812877dd112eb6da33ea8585a064e25ff4e3cd6
zszfympx <2792668017@qq.com>
2024-01-07 19:45:17 +0800

    weave loader download

--- src/main/java/org/cubewhy/celestial/game/addon/WeaveMod.java
+++ src/main/java/org/cubewhy/celestial/game/addon/WeaveMod.java
@@ -6,8 +6,13 @@
 
 package org.cubewhy.celestial.game.addon;
 
+import com.google.gson.Gson;
 import lombok.Getter;
+import okhttp3.Response;
+import org.cubewhy.celestial.entities.Assets;
+import org.cubewhy.celestial.entities.releaseEntity;
 import org.cubewhy.celestial.game.BaseAddon;
+import org.cubewhy.celestial.utils.RequestUtils;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -52,5 +57,38 @@ public class WeaveMod extends BaseAddon {
     @Override
     public String toString() {
         return this.file.getName();
+    }
+
+    public boolean downloadWeaveLoader(@Nullable String loaderUrl){
+        if(loaderUrl == null){
+            String api_json;
+            try(Response response = RequestUtils.get("https://api.github.com/repos/Weave-MC/Weave-Loader/releases/latest").execute()){
+                assert response.body() != null;
+                api_json = response.body().string();
+            } catch (IOException e) {
+                return false;
+            }
+            releaseEntity releaseEntity = JsonToObj(api_json, org.cubewhy.celestial.entities.releaseEntity.class);
+            if (releaseEntity != null) {
+                Assets[] assets = releaseEntity.getAssets().toArray(new Assets[0]);
+                for(Assets i:assets){
+                    if(i.getName().endsWith(".jar")){
+                        //TODO: download i.browser_download_url() to ~/.cubewhy/addon/Weave-<%s>.jar, releaseEntity.getName()
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public static <T> T JsonToObj(String json, Class<T> clz){
+        Gson gson = new Gson();
+        if(Objects.isNull(json)) return null;
+        T obj = gson.fromJson(json, clz);
+        if(Objects.isNull(obj)){
+            return null;
+        }else{
+            return obj;
+        }
     }
 }
