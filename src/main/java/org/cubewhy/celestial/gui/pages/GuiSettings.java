816ad7cda1a951ddffb8de6625c39be7ab7d2543
cubewhy <me@lunarclient.top>
2024-01-13 09:24:07 +0800

    select vm

--- src/main/java/org/cubewhy/celestial/gui/pages/GuiSettings.java
+++ src/main/java/org/cubewhy/celestial/gui/pages/GuiSettings.java
@@ -2,17 +2,21 @@ package org.cubewhy.celestial.gui.pages;
 
 import lombok.extern.slf4j.Slf4j;
 import org.cubewhy.celestial.gui.layouts.VerticalFlowLayout;
+import org.cubewhy.celestial.utils.GuiUtils;
 import org.cubewhy.celestial.utils.SystemUtils;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
 import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.cubewhy.celestial.Celestial.config;
 import static org.cubewhy.celestial.Celestial.f;
+import static org.cubewhy.celestial.gui.GuiLauncher.statusBar;
 
 @Slf4j
 public class GuiSettings extends JScrollPane {
@@ -36,10 +40,31 @@ public class GuiSettings extends JScrollPane {
 
         String customJre = config.getValue("jre").getAsString();
         JButton btnSelectPath = new JButton((customJre.isEmpty()) ? SystemUtils.getCurrentJavaExec().getPath() : customJre);
+        JButton btnUnset = new JButton(f.getString("gui.settings.jvm.jre.unset"));
+        btnSelectPath.addActionListener((e) -> {
+            File file = GuiUtils.chooseFile(new FileNameExtensionFilter("Java Executable", "exe"));
+            if (file != null) {
+                JButton source = (JButton) e.getSource();
+                statusBar.setText(String.format(f.getString("gui.settings.jvm.jre.success"), file));
+                config.setValue("jre", file.getPath());
+                source.setText(file.getPath());
+            }
+        });
+        btnUnset.addActionListener((e) -> {
+            if (JOptionPane.showConfirmDialog(this, f.getString("gui.settings.jvm.jre.unset.confirm"), "Confirm", JOptionPane.YES_NO_OPTION) == JOptionPane.NO_OPTION) {
+                return;
+            }
+            JButton source = (JButton) e.getSource();
+            File java = SystemUtils.getCurrentJavaExec();
+            btnSelectPath.setText(java.getPath());
+            config.setValue("jre", "");
+            statusBar.setText(f.getString("gui.settings.jvm.jre.unset.success"));
+        });
         // jre settings
         JPanel p1 = new JPanel();
         p1.add(new JLabel(f.getString("gui.settings.jvm.jre")));
         p1.add(btnSelectPath);
+        p1.add(btnUnset);
         panelVM.add(p1);
         // ram settings
         JPanel p2 = new JPanel();
