9d74cfcc2e86fcbfd55959d57aed7624448d10be
cubewhy <me@lunarclient.top>
2024-01-13 09:08:32 +0800

    ram settings

--- src/main/java/org/cubewhy/celestial/utils/SystemUtils.java
+++ src/main/java/org/cubewhy/celestial/utils/SystemUtils.java
@@ -6,18 +6,22 @@
 
 package org.cubewhy.celestial.utils;
 
+import com.sun.management.OperatingSystemMXBean;
 import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
 // from hmcl launcher
 @Slf4j
 public final class SystemUtils {
-    private SystemUtils() {}
+    private SystemUtils() {
+    }
 
     public static Process callExternalProcess(ProcessBuilder processBuilder) throws IOException, InterruptedException {
         ManagedProcess managedProcess = new ManagedProcess(processBuilder);
@@ -34,5 +38,21 @@ public final class SystemUtils {
             }
         }
         return null;
+    }
+
+    public static File getCurrentJavaExec() {
+        String exec = System.getProperty("java.home") + "/bin/java";
+        if (OSEnum.getCurrent().equals(OSEnum.Windows)) {
+            exec += ".exe";
+        }
+        return new File(exec);
+    }
+
+    /**
+     * Get total RAM (MB)
+     */
+    public static int getTotalMem() {
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        return (int) (osBean.getTotalMemorySize() / 1048576);
     }
 }
