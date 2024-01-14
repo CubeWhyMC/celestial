40bb945fba7d418c3c84d23e92244c330436980c
cubewhy <me@lunarclient.top>
2024-01-14 14:23:02 +0800

    lunarcn mod

--- src/main/java/org/cubewhy/celestial/gui/pages/GuiPlugins.java
+++ src/main/java/org/cubewhy/celestial/gui/pages/GuiPlugins.java
@@ -11,6 +11,7 @@ import org.cubewhy.celestial.files.DownloadManager;
 import org.cubewhy.celestial.files.Downloadable;
 import org.cubewhy.celestial.game.RemoteAddon;
 import org.cubewhy.celestial.game.addon.JavaAgent;
+import org.cubewhy.celestial.game.addon.LunarCNMod;
 import org.cubewhy.celestial.game.addon.WeaveMod;
 import org.cubewhy.celestial.gui.layouts.VerticalFlowLayout;
 import org.jetbrains.annotations.NotNull;
@@ -62,17 +63,24 @@ public class GuiPlugins extends JPanel {
         panelWeave.setLayout(new VerticalFlowLayout());
         JPanel panelAgents = new JPanel();
         panelAgents.setLayout(new VerticalFlowLayout());
+        JPanel panelCN = new JPanel();
+        panelCN.setLayout(new VerticalFlowLayout());
         JScrollPane sp1 = new JScrollPane(panelWeave, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         sp1.getVerticalScrollBar().setUnitIncrement(30);
         JScrollPane sp2 = new JScrollPane(panelAgents, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         sp2.getVerticalScrollBar().setUnitIncrement(30);
+        JScrollPane sp3 = new JScrollPane(panelCN, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        sp3.getVerticalScrollBar().setUnitIncrement(30);
         this.tab.addTab("Weave", sp1);
         this.tab.addTab("Agents", sp2);
+        this.tab.addTab("LunarCN", sp3);
         for (RemoteAddon addon : addons) {
             if (addon.getCategory() == RemoteAddon.Category.WEAVE) {
                 addPlugin(panelWeave, addon, WeaveMod.modFolder);
             } else if (addon.getCategory() == RemoteAddon.Category.AGENT) {
                 addPlugin(panelAgents, addon, JavaAgent.javaAgentFolder);
+            } else if (addon.getCategory() == RemoteAddon.Category.CN) {
+                addPlugin(panelCN, addon, LunarCNMod.modFolder);
             }
         }
     }
@@ -80,10 +88,10 @@ public class GuiPlugins extends JPanel {
     /**
      * Add label and download button
      *
-     * @param panel the panel
-     * @param addon the addon
+     * @param panel  the panel
+     * @param addon  the addon
      * @param folder target folder
-     * */
+     */
     private void addPlugin(JPanel panel, @NotNull RemoteAddon addon, File folder) {
         JPanel p = new JPanel();
         p.setLayout(new GridLayout());
@@ -91,14 +99,13 @@ public class GuiPlugins extends JPanel {
         File file = new File(folder, addon.getName());
         if (file.exists()) {
             p.add(new JLabel(f.getString("gui.plugin.exist")));
-        }
-        else {
+        } else {
             p.add(getDownloadButton(addon.getDownloadURL(), file));
         }
         panel.add(p);
     }
 
-    private @NotNull JButton getDownloadButton(URL url, File file) {
+    private @NotNull JButton getDownloadButton(URL url, @NotNull File file) {
         JButton btn = new JButton(String.format(f.getString("gui.plugins.download"), file.getName()));
         btn.addActionListener((e) -> {
             DownloadManager.download(new Downloadable(url, file, null));
