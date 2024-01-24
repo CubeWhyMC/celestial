c47ea2593a60426f148275bd989567444001e2fd
cubewhy <me@lunarclient.top>
2024-01-24 12:32:20 +0800

    ONLY 1 LICENSE TEXT

---
+++ src/main/kotlin/org/cubewhy/celestial/gui/dialogs/AddonInfoDialog.kt
@@ -0,0 +1,14 @@
+/*
+ * Celestial Launcher <me@lunarclient.top>
+ * License under GPLv3
+ * Do NOT remove this note if you want to copy this file.
+ */
+
+package org.cubewhy.celestial.gui.dialogs
+
+import com.google.gson.JsonObject
+import javax.swing.JDialog
+
+class AddonInfoDialog(json: JsonObject) : JDialog() {
+
+}
