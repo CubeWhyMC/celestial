07f2450a050ea4090581197a405e28537bd25d92
cubewhy <me@lunarclient.top>
2024-01-24 12:32:20 +0800

    ONLY 1 LICENSE TEXT

--- src/main/kotlin/org/cubewhy/celestial/files/Downloadable.kt
+++ src/main/kotlin/org/cubewhy/celestial/files/Downloadable.kt
@@ -3,12 +3,6 @@
  * License under GPLv3
  * Do NOT remove this note if you want to copy this file.
  */
-
-/*
- * Celestial Launcher <me@lunarclient.top>
- * License under GPLv3
- * Do NOT remove this note if you want to copy this file.
- */
 package org.cubewhy.celestial.files
 
 import java.io.File
--- src/main/kotlin/org/cubewhy/celestial/gui/dialogs/MirrorDialog.kt
+++ src/main/kotlin/org/cubewhy/celestial/gui/dialogs/MirrorDialog.kt
@@ -3,12 +3,6 @@
  * License under GPLv3
  * Do NOT remove this note if you want to copy this file.
  */
-
-/*
- * Celestial Launcher <me@lunarclient.top>
- * License under GPLv3
- * Do NOT remove this note if you want to copy this file.
- */
 package org.cubewhy.celestial.gui.dialogs
 
 import cn.hutool.core.util.NumberUtil
