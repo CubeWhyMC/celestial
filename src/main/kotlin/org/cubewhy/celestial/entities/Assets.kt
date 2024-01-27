5cb05a2e5be745998264d189d939e1786884168f
cubewhy <me@lunarclient.top>
2024-01-27 18:01:37 +0800

    cleanup code

--- src/main/kotlin/org/cubewhy/celestial/entities/Assets.kt
+++ src/main/kotlin/org/cubewhy/celestial/entities/Assets.kt
@@ -20,82 +20,49 @@ class Assets {
     lateinit var updated_at: Date
     lateinit var browser_download_url: String
 
-    override fun equals(o: Any?): Boolean {
-        if (o === this) return true
-        if (o !is Assets) return false
-        val other = o
-        if (!other.canEqual(this as Any)) return false
-        val `this$url`: Any = this.url
-        val `other$url`: Any = other.url
-        if (if (`this$url` == null) `other$url` != null else `this$url` != `other$url`) return false
-        if (this.id != other.id) return false
-        val `this$node_id`: Any = this.node_id
-        val `other$node_id`: Any = other.node_id
-        if (if (`this$node_id` == null) `other$node_id` != null else `this$node_id` != `other$node_id`) return false
-        val `this$name`: Any = this.name
-        val `other$name`: Any = other.name
-        if (if (`this$name` == null) `other$name` != null else `this$name` != `other$name`) return false
-        val `this$label`: Any = this.label
-        val `other$label`: Any = other.label
-        if (if (`this$label` == null) `other$label` != null else `this$label` != `other$label`) return false
-        val `this$uploader`: Any = this.uploader
-        val `other$uploader`: Any = other.uploader
-        if (if (`this$uploader` == null) `other$uploader` != null else `this$uploader` != `other$uploader`) return false
-        val `this$content_type`: Any = this.content_type
-        val `other$content_type`: Any = other.content_type
-        if (if (`this$content_type` == null) `other$content_type` != null else `this$content_type` != `other$content_type`) return false
-        val `this$state`: Any = this.state
-        val `other$state`: Any = other.state
-        if (if (`this$state` == null) `other$state` != null else `this$state` != `other$state`) return false
-        if (this.size != other.size) return false
-        if (this.download_count != other.download_count) return false
-        val `this$created_at`: Any = this.created_at
-        val `other$created_at`: Any = other.created_at
-        if (if (`this$created_at` == null) `other$created_at` != null else `this$created_at` != `other$created_at`) return false
-        val `this$updated_at`: Any = this.updated_at
-        val `other$updated_at`: Any = other.updated_at
-        if (if (`this$updated_at` == null) `other$updated_at` != null else `this$updated_at` != `other$updated_at`) return false
-        val `this$browser_download_url`: Any = this.browser_download_url
-        val `other$browser_download_url`: Any = other.browser_download_url
-        return !if (`this$browser_download_url` == null) `other$browser_download_url` != null else `this$browser_download_url` != `other$browser_download_url`
-    }
 
-    protected fun canEqual(other: Any?): Boolean {
-        return other is Assets
-    }
-
-    override fun hashCode(): Int {
-        val PRIME = 59
-        var result = 1
-        val `$url`: Any = this.url
-        result = result * PRIME + `$url`.hashCode()
-        val `$id` = this.id
-        result = result * PRIME + (`$id` ushr 32 xor `$id`).toInt()
-        val `$node_id`: Any = this.node_id
-        result = result * PRIME + `$node_id`.hashCode()
-        val `$name`: Any = this.name
-        result = result * PRIME + `$name`.hashCode()
-        val `$label`: Any = this.label
-        result = result * PRIME + `$label`.hashCode()
-        val `$uploader`: Any = this.uploader
-        result = result * PRIME + `$uploader`.hashCode()
-        val `$content_type`: Any = this.content_type
-        result = result * PRIME + `$content_type`.hashCode()
-        val `$state`: Any = this.state
-        result = result * PRIME + `$state`.hashCode()
-        val `$size` = this.size
-        result = result * PRIME + (`$size` ushr 32 xor `$size`).toInt()
-        result = result * PRIME + this.download_count
-        val `$created_at`: Any = this.created_at
-        result = result * PRIME + `$created_at`.hashCode()
-        val `$updated_at`: Any = this.updated_at
-        result = result * PRIME + `$updated_at`.hashCode()
-        val `$browser_download_url`: Any = this.browser_download_url
-        result = result * PRIME + `$browser_download_url`.hashCode()
-        return result
-    }
 
     override fun toString(): String {
         return "Assets(url=" + this.url + ", id=" + this.id + ", node_id=" + this.node_id + ", name=" + this.name + ", label=" + this.label + ", uploader=" + this.uploader + ", content_type=" + this.content_type + ", state=" + this.state + ", size=" + this.size + ", download_count=" + this.download_count + ", created_at=" + this.created_at + ", updated_at=" + this.updated_at + ", browser_download_url=" + this.browser_download_url + ")"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Assets
+
+        if (url != other.url) return false
+        if (id != other.id) return false
+        if (node_id != other.node_id) return false
+        if (name != other.name) return false
+        if (label != other.label) return false
+        if (uploader != other.uploader) return false
+        if (content_type != other.content_type) return false
+        if (state != other.state) return false
+        if (size != other.size) return false
+        if (download_count != other.download_count) return false
+        if (created_at != other.created_at) return false
+        if (updated_at != other.updated_at) return false
+        if (browser_download_url != other.browser_download_url) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = url.hashCode()
+        result = 31 * result + id.hashCode()
+        result = 31 * result + node_id.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + label.hashCode()
+        result = 31 * result + uploader.hashCode()
+        result = 31 * result + content_type.hashCode()
+        result = 31 * result + state.hashCode()
+        result = 31 * result + size.hashCode()
+        result = 31 * result + download_count
+        result = 31 * result + created_at.hashCode()
+        result = 31 * result + updated_at.hashCode()
+        result = 31 * result + browser_download_url.hashCode()
+        return result
     }
 }
--- src/main/kotlin/org/cubewhy/celestial/entities/Author.kt
+++ src/main/kotlin/org/cubewhy/celestial/entities/Author.kt
@@ -29,7 +29,58 @@ class Author {
     lateinit var type: String
     var isSite_admin: Boolean = false
 
+
     override fun toString(): String {
         return "Author(login=" + this.login + ", id=" + this.id + ", node_id=" + this.node_id + ", avatar_url=" + this.avatar_url + ", gravatar_id=" + this.gravatar_id + ", url=" + this.url + ", html_url=" + this.html_url + ", followers_url=" + this.followers_url + ", following_url=" + this.following_url + ", gists_url=" + this.gists_url + ", starred_url=" + this.starred_url + ", subscriptions_url=" + this.subscriptions_url + ", organizations_url=" + this.organizations_url + ", repos_url=" + this.repos_url + ", events_url=" + this.events_url + ", received_events_url=" + this.received_events_url + ", type=" + this.type + ", site_admin=" + this.isSite_admin + ")"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Author
+
+        if (login != other.login) return false
+        if (id != other.id) return false
+        if (node_id != other.node_id) return false
+        if (avatar_url != other.avatar_url) return false
+        if (gravatar_id != other.gravatar_id) return false
+        if (url != other.url) return false
+        if (html_url != other.html_url) return false
+        if (followers_url != other.followers_url) return false
+        if (following_url != other.following_url) return false
+        if (gists_url != other.gists_url) return false
+        if (starred_url != other.starred_url) return false
+        if (subscriptions_url != other.subscriptions_url) return false
+        if (organizations_url != other.organizations_url) return false
+        if (repos_url != other.repos_url) return false
+        if (events_url != other.events_url) return false
+        if (received_events_url != other.received_events_url) return false
+        if (type != other.type) return false
+        if (isSite_admin != other.isSite_admin) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = login.hashCode()
+        result = 31 * result + id.hashCode()
+        result = 31 * result + node_id.hashCode()
+        result = 31 * result + avatar_url.hashCode()
+        result = 31 * result + gravatar_id.hashCode()
+        result = 31 * result + url.hashCode()
+        result = 31 * result + html_url.hashCode()
+        result = 31 * result + followers_url.hashCode()
+        result = 31 * result + following_url.hashCode()
+        result = 31 * result + gists_url.hashCode()
+        result = 31 * result + starred_url.hashCode()
+        result = 31 * result + subscriptions_url.hashCode()
+        result = 31 * result + organizations_url.hashCode()
+        result = 31 * result + repos_url.hashCode()
+        result = 31 * result + events_url.hashCode()
+        result = 31 * result + received_events_url.hashCode()
+        result = 31 * result + type.hashCode()
+        result = 31 * result + isSite_admin.hashCode()
+        return result
     }
 }
