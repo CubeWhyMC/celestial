592bca1526563073d01defcb53eb29e0b5bd8cb4
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-02-02 15:03:07 +0800

    remove useless things

--- src/main/kotlin/co/gongzh/procbridge/ServerException.kt
+++ src/main/kotlin/co/gongzh/procbridge/ServerException.kt
@@ -1,9 +1,6 @@
 package co.gongzh.procbridge
 
-class ServerException : RuntimeException {
-    internal constructor(message: String?) : super(message ?: UNKNOWN_SERVER_ERROR)
-
-    internal constructor(cause: Throwable) : super(cause.message, cause)
+class ServerException internal constructor(cause: Throwable) : RuntimeException(cause.message, cause) {
 
     companion object {
         private const val UNKNOWN_SERVER_ERROR = "unknown server error"
