f5b471b109fa496df82d961ca79eebe80eb51036
cubewhy <cubewhy@163.com>
2024-02-04 13:47:19 +0800

    use kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.+"
+    id ("com.gorylenko.gradle-git-properties") version "2.4.1"
+    kotlin("jvm")
+    kotlin("plugin.serialization") version "1.9.0"
+}
+
+group = "org.cubewhy.celestial"
+version = "2.1-pre1-SNAPSHOT"
+
+println("Java: " + System.getProperty("java.version") + " JVM: " + System.getProperty("java.vm.version") + "(" + System.getProperty("java.vendor") + ") Arch: " + System.getProperty("os.arch"))
+println("Celestial Launcher -> https://www.lunarclient.top/")
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+
+repositories {
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://repo.typesafe.com/typesafe/maven-releases/")
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.11")
+    implementation("org.slf4j:slf4j-log4j12:2.0.11")
+    implementation("org.apache.logging.log4j:log4j-api:2.22.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("net.sf.jopt-simple:jopt-simple:6.0-alpha-3")
+    implementation("com.formdev:flatlaf:3.4-SNAPSHOT")
+    implementation("commons-io:commons-io:2.15.1")
+    implementation("cn.hutool:hutool-crypto:5.8.25")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("fatjar")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    exclude("native-binaries/**")
+
+    exclude("LICENSE.txt")
+
+    exclude("META-INF/maven/**")
+    exclude("META-INF/versions/**")
+
+    exclude("org/junit/**")
+}
+
+tasks.jar {
+    dependsOn("shadowJar")
+
+    manifest {
+        attributes(
+            "Main-Class" to "org.cubewhy.celestial.Celestial"
+        )
+        attributes(
+            "Charset" to "UTF-8"
+        )
+    }
+}
+
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "1.9.22"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    }
+}
+rootProject.name = "celestial"
+
